7ae93b5c6aedfb04b81fe25e3497099250e4293f
DPM3 <dpm3git@gmail.com>
2020-08-30 18:48:37 +0300

    cleaned things in filesystem

--- src/src/file_reading.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <fstream>
-#include <iterator>
-#include <string>
-
-std::string readFileContent(const std::string& filePath);
-
-void writeFileContent(const std::string& filePath, const std::string& content);
